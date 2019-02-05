91301bb2e86c3c73da89513233745805c65b0edb
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-02-05 21:36:05 +0100

    isSamePosition function check coords, return true/false

--- script.js
+++ script.js
@@ -19,37 +19,37 @@ function tanksGame() {
         }
     }
     createRow(50,50,100);
-    // createRow(50, 200, 100);
-    // createRow(100, 400, 300);
-    // createRow(100, 300, 400);
-    // createRow(100, 200, 200);
-    // createRow(250, 400, 500);
-    // createRow(350, 450, 700);
-    // createRow(350, 450, 600);
-    // createRow(400, 600, 0);
-    // createRow(400, 700, 100);
-    // createRow(400, 600, 200);
-    // createRow(400, 450, 400);
-    // createRow(500, 550, 300);
-    // createRow(500, 550, 450);
-    // createRow(550, 600, 350);
-    // createRow(650, 850, 650);
-    // createRow(650, 850, 450);
-    // createRow(650, 850, 550);
-    // createRow(700, 800, 50);
-    // createRow(700, 850, 350);
-    // createRow(950, 1150, 650);
-    // createRow(1050, 1150, 550);
-    // createRow(1050, 1150, 450);
-    // createRow(1050, 1100, 350);
-    // createRow(1050, 1100, 250);
-    // createRow(1150, 1350, 100);
-    // createRow(1150, 1250, 0);
-    // createRow(1250, 1350, 600);
-    // createRow(1250, 1350, 400);
-    // createRow(1250, 1350, 500);
-    // createRow(1250, 1350, 300);
-    // createRow(1250, 1350, 200);
+    createRow(50, 200, 100);
+    createRow(100, 400, 300);
+    createRow(100, 300, 400);
+    createRow(100, 200, 200);
+    createRow(250, 400, 500);
+    createRow(350, 450, 700);
+    createRow(350, 450, 600);
+    createRow(400, 600, 0);
+    createRow(400, 700, 100);
+    createRow(400, 600, 200);
+    createRow(400, 450, 400);
+    createRow(500, 550, 300);
+    createRow(500, 550, 450);
+    createRow(550, 600, 350);
+    createRow(650, 850, 650);
+    createRow(650, 850, 450);
+    createRow(650, 850, 550);
+    createRow(700, 800, 50);
+    createRow(700, 850, 350);
+    createRow(950, 1150, 650);
+    createRow(1050, 1150, 550);
+    createRow(1050, 1150, 450);
+    createRow(1050, 1100, 350);
+    createRow(1050, 1100, 250);
+    createRow(1150, 1350, 100);
+    createRow(1150, 1250, 0);
+    createRow(1250, 1350, 600);
+    createRow(1250, 1350, 400);
+    createRow(1250, 1350, 500);
+    createRow(1250, 1350, 300);
+    createRow(1250, 1350, 200);
 
     function createColumn(startY, endY, x) {
         for (let y = startY; y <= endY; y += 50) {
@@ -60,22 +60,22 @@ function tanksGame() {
             gameContainer.append(building);
         }
     }
-    // createColumn(200, 300, 0);
-    // createColumn(400, 550, 50);
-    // createColumn(650, 700, 50);
-    // createColumn(500, 700, 150);
-    // createColumn(600, 650, 250);
-    // createColumn(0, 100, 300);
-    // createColumn(550, 700, 550);
-    // createColumn(200, 250, 650);
-    // createColumn(200, 250, 750);
-    // createColumn(650, 700, 850);
-    // createColumn(150, 250, 850);
-    // createColumn(50, 150, 900);
-    // createColumn(150, 350, 950);
-    // createColumn(450, 650, 950);
-    // createColumn(50, 150, 1050);
-    // createColumn(250, 350, 1150);
+    createColumn(200, 300, 0);
+    createColumn(400, 550, 50);
+    createColumn(650, 700, 50);
+    createColumn(500, 700, 150);
+    createColumn(600, 650, 250);
+    createColumn(0, 100, 300);
+    createColumn(550, 700, 550);
+    createColumn(200, 250, 650);
+    createColumn(200, 250, 750);
+    createColumn(650, 700, 850);
+    createColumn(150, 250, 850);
+    createColumn(50, 150, 900);
+    createColumn(150, 350, 950);
+    createColumn(450, 650, 950);
+    createColumn(50, 150, 1050);
+    createColumn(250, 350, 1150);
 
     let buildings = document.querySelectorAll('.building');
     let buildingsPosition = [];
@@ -110,10 +110,10 @@ function tanksGame() {
     }
     tank1.createTank();
 
-    let isSamePosition = buildingsPosition.positionX === tank1.positionX ? true : false;
-
-    if (isSamePosition) {
-        console.log('TANK ON THE OBSTACLE');
+    function isSamePosition() {
+        return buildingsPosition.some(el => {
+            return el.positionX === tank1.positionX && el.positionY === tank1.positionY
+        })
     }
 
     function moveTank1(e){
@@ -121,21 +121,25 @@ function tanksGame() {
             tank1.node.style.transform = 'rotate(90deg)';
             tank1.positionX += 50;
             tank1.node.style.left = `${tank1.positionX}px`;
-            console.log(tank1);
+                console.log(isSamePosition());
+                console.log(tank1);
         } else if (e.keyCode === 37 ) { // move 50px left
             tank1.node.style.transform = 'rotate(270deg)';
             tank1.positionX -= 50;
             tank1.node.style.left = `${tank1.positionX}px`;
+                console.log(isSamePosition());
                 console.log(tank1);
         } else if (e.keyCode === 38 ) { // move 50px top
             tank1.node.style.transform = 'rotate(0deg)';
             tank1.positionY -= 50;
             tank1.node.style.top = `${tank1.positionY}px`;
+                console.log(isSamePosition());
                 console.log(tank1);
         } else if (e.keyCode === 40 ) { // move 50px down
             tank1.node.style.transform = 'rotate(180deg)';
             tank1.positionY += 50;
             tank1.node.style.top = `${tank1.positionY}px`;
+                console.log(isSamePosition());
                 console.log(tank1);
         }
     }
